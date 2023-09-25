176d14d9754bf109ab37a9bbd5a0fae9c87b8ba2
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-25 15:58:35 -0300

    feat: update ProductGroupsController class

--- src/main/java/com/lacertech/lacereats/registrations/productgroups/controllers/ProductGroupsController.java
+++ src/main/java/com/lacertech/lacereats/registrations/productgroups/controllers/ProductGroupsController.java
@@ -3,6 +3,8 @@ package com.lacertech.lacereats.registrations.productgroups.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,42 +14,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lacertech.lacereats.controllers.interfaces.CommonController;
 import com.lacertech.lacereats.dto.productgroupsDTO.ProductGroupsDTO;
+import com.lacertech.lacereats.dto.productgroupsDTO.ProductGroupsResponseDTO;
 import com.lacertech.lacereats.registrations.productgroups.services.ProductGroupService;
 
+import jakarta.validation.Valid;
+
 @RestController
-@RequestMapping(path = "/registrations/product groups")
-public class ProductGroupsController {
+@RequestMapping(path = "/registrations/product groups", produces = "application/json")
+public class ProductGroupsController implements CommonController<ProductGroupsResponseDTO, ProductGroupsDTO> {
 
     @Autowired
     private ProductGroupService productGroupService;
     
     @GetMapping
-    public List<ProductGroupsDTO> searchAll(){
-        return productGroupService.searchAll();
+    public ResponseEntity<List<ProductGroupsResponseDTO>> searchAll(){
+        return new ResponseEntity<List<ProductGroupsResponseDTO>>(productGroupService.searchAll(), HttpStatus.OK);
     }
 
     @PostMapping
-    public void register(@RequestBody ProductGroupsDTO ProductGroup) {
-        productGroupService.create(ProductGroup);
-        return;
+    public ResponseEntity<ProductGroupsResponseDTO> register(@RequestBody @Valid ProductGroupsDTO ProductGroup) {
+        ProductGroupsResponseDTO newProductGroup = productGroupService.post(ProductGroup);
+        return new ResponseEntity<>(newProductGroup, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ProductGroupsDTO searchById(@PathVariable Integer id) {
-        return productGroupService.searchById(id);
+    public ResponseEntity<ProductGroupsResponseDTO> searchById(@PathVariable Integer id) {
+        return new ResponseEntity<>(productGroupService.searchById(id), HttpStatus.OK);
     }
 
-
-    @PutMapping
-    public void update(@RequestBody ProductGroupsDTO productGroup) {
-        productGroupService.update(productGroup);
-        return;
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductGroupsResponseDTO> update(@PathVariable Integer id, @RequestBody @Valid ProductGroupsDTO productGroup) {
+        return new ResponseEntity<>(productGroupService.update(id, productGroup), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id) {
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         productGroupService.delete(id);
-        return;
+        return ResponseEntity.noContent().build();
     }
 }
