f92422173d2a510eac44f4c3752ff889179d6eef
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-25 14:39:14 -0300

    feat: update RoleController

--- src/main/java/com/lacertech/lacereats/registrations/roles/controllers/RoleController.java
+++ src/main/java/com/lacertech/lacereats/registrations/roles/controllers/RoleController.java
@@ -2,11 +2,18 @@ package com.lacertech.lacereats.registrations.roles.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lacertech.lacereats.controllers.interfaces.CommonController;
+import com.lacertech.lacereats.database.model.RolesModel;
 import com.lacertech.lacereats.dto.rolesDTO.RoleDTO;
+import com.lacertech.lacereats.dto.rolesDTO.RoleResponseDTO;
 import com.lacertech.lacereats.registrations.roles.services.RoleService;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,53 +24,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping(path = "/registrations/role")
-public class RoleController {
+@RequestMapping(path = "/registrations/role", produces = "application/json")
+public class RoleController implements CommonController<RoleResponseDTO, RoleDTO> {
 
     @Autowired
     private RoleService roleService;
-    
+
     @GetMapping
-    public List<RoleDTO> searchAllRoles(){
-        return roleService.getRole();
+    public ResponseEntity<List<RoleResponseDTO>> searchAll(){
+        return new ResponseEntity<>(roleService.searchAll(), HttpStatus.OK);
     }
 
     @PostMapping
-    public void registerRole(@RequestBody RoleDTO role) {
-        roleService.postRole(role.role());
-        return;
+    public ResponseEntity<RoleResponseDTO> register(@RequestBody @Valid RoleDTO role) {
+        RoleResponseDTO creatingNewRole = roleService.post(role);
+        return new ResponseEntity<>(creatingNewRole, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public RoleDTO searchRoleById(@PathVariable Integer id) {
-        return roleService.searchRoleById(id);
-    }
-
-
-    @PutMapping
-    public void updateRole(@RequestBody RoleDTO role) {
-        roleService.updateRole(role);
-        return;
+    public ResponseEntity<RoleResponseDTO> searchById(@PathVariable Integer id) {
+        return new ResponseEntity<>(roleService.searchById(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public void updateRole(@PathVariable Integer id,@RequestBody RoleDTO role) {
-        if (role.role() != null) {
-            roleService.updateRole(id, role.role());
-            return;
-        }
-
-        if (role.status() != null) {
-            roleService.updateRole(id, role.status());
-            return;
-        }
+    public ResponseEntity<RoleResponseDTO> update(@PathVariable Integer id,@RequestBody @Valid RoleDTO role) {
+            return new ResponseEntity<>(roleService.update(id, role), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteRole(@PathVariable Integer id) {
-        roleService.deleteRole(id);
-        return;
+    public ResponseEntity<Void> deleteRole(@PathVariable Integer id) {
+        roleService.delete(id);
+        return ResponseEntity.noContent().build();
     }
-
-
 }
