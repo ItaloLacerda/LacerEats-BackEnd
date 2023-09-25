15d3d0ae02d5065fed0d0372fedb0300c0fcc472
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-25 15:57:22 -0300

    feat : creating existsByDescription method in ProductGroupRepository

--- src/main/java/com/lacertech/lacereats/database/repository/ProductGroupRepository.java
+++ src/main/java/com/lacertech/lacereats/database/repository/ProductGroupRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lacertech.lacereats.database.model.ProductGroupModel;
 
 public interface ProductGroupRepository extends JpaRepository<ProductGroupModel, Integer> {
-    
+    Boolean existsByDescription(String description);
 }
