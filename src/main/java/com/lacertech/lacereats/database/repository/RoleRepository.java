6fdf93457d569758c02e9cda09430d32f947d958
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-25 14:37:04 -0300

    feat: Update RoleRepository

--- src/main/java/com/lacertech/lacereats/database/repository/RoleRepository.java
+++ src/main/java/com/lacertech/lacereats/database/repository/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lacertech.lacereats.database.model.RolesModel;
 
 public interface RoleRepository extends JpaRepository<RolesModel, Integer> {
-    
+    Boolean existsByRole(String role);
 }
