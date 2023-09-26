fd22265e47dc46c6bacaac26ef7a6272b5462278
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-26 14:30:28 -0300

    style: adapts ProductModel to Checkstyle:

--- src/main/java/com/lacertech/lacereats/database/model/ProductModel.java
+++ src/main/java/com/lacertech/lacereats/database/model/ProductModel.java
@@ -1,7 +1,5 @@
 package com.lacertech.lacereats.database.model;
 
-import java.util.Set;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,140 +9,113 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
+
+/**
+ * This class represents a product model in the system.
+ * A product has various information including description, barcode, cost price,
+ * selling price, product group, status and associated suppliers.
+ *
+ * @author Italo Lacerda
+ * @since 2.0
+ */
+@Getter
+@Setter
 @Entity
 @Table(name = "products")
 public class ProductModel {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id_products")
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id_products")
+  private Integer id;
 
-    @Column(length = 90, nullable = false, unique = true)
-    private String description;
+  @Column(length = 90, nullable = false, unique = true)
+  private String description;
 
-    @Column
-    private String ncm;
+  @Column
+  private String ncm;
 
-    @Column(name = "cost_price", nullable = false)
-    private Float costPrice; 
+  @Column(name = "cost_price", nullable = false)
+  private Float costPrice;
 
-    @Column(name = "sale_price", nullable = false)
-    private Float salePrice;
-    
-    @Column(length = 45, name = "bar_code", nullable = false, unique = true)
-    private String barCode;
+  @Column(name = "sale_price", nullable = false)
+  private Float salePrice;
+  
+  @Column(length = 45, name = "bar_code", unique = true)
+  private String barCode;
 
-    @Column(nullable = false)
-    private Boolean status;
+  @Column(nullable = false)
+  private Boolean status;
 
-    @ManyToOne
-    @JoinColumn(name = "id_product_group", nullable = false)
-    private ProductGroupModel productGroup;
+  @ManyToOne
+  @JoinColumn(name = "id_product_group", nullable = false)
+  private ProductGroupModel productGroup;
 
-    @ManyToMany(mappedBy = "products")
-    private Set<SuppliersModel> suppliers;
+  @ManyToMany(mappedBy = "products")
+  private Set<SuppliersModel> suppliers;
 
-    public ProductModel() {}
+  public ProductModel() {}
+  /**
+   * Constructor to create a new product.
+   *
+   * @param description The product description.
+   * @param barCode The product barcode.
+   * @param costPrice The product cost price.
+   * @param salePrice The product sale price.
+   * @param productGroup The product group to which the product belongs.
+   * @param status The product status.
+   */
 
-    public ProductModel(
-        String description,
-        String barCode,
-        Float costPrice,
-        Float salePrice,
-        ProductGroupModel productGroup,
-        Boolean status
-        ) {
-        this.description = description;
-        this.barCode = barCode;
-        this.costPrice = costPrice;
-        this.salePrice = salePrice;
-        this.productGroup = productGroup;
-        this.status = status;    
-    }
+  public ProductModel(
+      String description,
+      String barCode,
+      Float costPrice,
+      Float salePrice,
+      ProductGroupModel productGroup,
+      Boolean status
+  ) {
+    this.description = description;
+    this.barCode = barCode;
+    this.costPrice = costPrice;
+    this.salePrice = salePrice;
+    this.productGroup = productGroup;
+    this.status = status;    
+  }
 
-    public ProductModel(Integer id, String description, String ncm,String barCode, Float costPrice, Float salePrice,
-            ProductGroupModel productGroup, Boolean status) {
-        this.id = id;
-        this.description = description;
-        this.ncm = ncm;
-        this.barCode = barCode;
-        this.costPrice = costPrice;
-        this.salePrice = salePrice;
-        this.productGroup = productGroup;
-        this.status = status;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getNcm() {
-        return ncm;
-    }
-
-    public void setNcm(String ncm) {
-        this.ncm = ncm;
-    }
-
-    public Float getCostPrice() {
-        return costPrice;
-    }
-
-    public void setCostPrice(Float costPrice) {
-        this.costPrice = costPrice;
-    }
-
-    public Float getSalePrice() {
-        return salePrice;
-    }
-
-    public void setSalePrice(Float salePrice) {
-        this.salePrice = salePrice;
-    }
-
-    public String getBarCode() {
-        return barCode;
-    }
-
-    public void setBarCode(String barCode) {
-        this.barCode = barCode;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public ProductGroupModel getProductGroup() {
-        return productGroup;
-    }
-
-    public void setProductGroup(ProductGroupModel productGroup) {
-        this.productGroup = productGroup;
-    }
-
-    public Set<SuppliersModel> getSuppliers() {
-        return suppliers;
-    }
-
-    public void setSuppliers(Set<SuppliersModel> suppliers) {
-        this.suppliers = suppliers;
-    }
+  /**
+   * Constructor to create a product with an existing ID.
+   *
+   * @param id The unique ID of the product.
+   * @param description The product description.
+   * @param ncm The NCM of the product.
+   * @param barCode The product barcode.
+   * @param costPrice The product cost price.
+   * @param salePrice The product sale price.
+   * @param productGroup The product group to which the product belongs.
+   * @param status The product status.
+   */
+  
+  public ProductModel(
+      Integer id, 
+      String description,
+      String ncm,
+      String barCode,
+      Float costPrice,
+      Float salePrice,
+      ProductGroupModel productGroup,
+      Boolean status
+  ) {
+    this.id = id;
+    this.description = description;
+    this.ncm = ncm;
+    this.barCode = barCode;
+    this.costPrice = costPrice;
+    this.salePrice = salePrice;
+    this.productGroup = productGroup;
+    this.status = status; 
+  }
 }
