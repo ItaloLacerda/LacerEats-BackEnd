8cf96f9fccd170236bc16670b7131bc458b3e4fe
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-27 01:32:23 -0300

    style: fix code style of CommonService interface

--- src/main/java/com/lacertech/lacereats/services/interfaces/CommonService.java
+++ src/main/java/com/lacertech/lacereats/services/interfaces/CommonService.java
@@ -2,13 +2,27 @@ package com.lacertech.lacereats.services.interfaces;
 
 import java.util.List;
 
+/**
+ * This interface defines common methods for services that handle business 
+ * logic and operations on resources.
+ *
+ * @param <ResponseDTO> The response DTO type representing the resource.
+ * @param <DTO> The DTO type used for creating or updating the resource.
+ */
 public interface CommonService<ResponseDTO, DTO> {
     
-    String getObjectName();
-    String getIdNotFoundMensage();
-    ResponseDTO post(DTO dto);
-    List<ResponseDTO> searchAll();
-    ResponseDTO searchById(Integer id);
-    ResponseDTO update(Integer id, DTO dto);
-    void delete(Integer id);
+  String getObjectName();
+
+  String getIdNotFoundMensage();
+
+  ResponseDTO post(DTO dto);
+
+  List<ResponseDTO> searchAll();
+
+  ResponseDTO searchById(Integer id);
+
+  ResponseDTO update(Integer id, DTO dto);
+
+  void delete(Integer id);
+
 }
