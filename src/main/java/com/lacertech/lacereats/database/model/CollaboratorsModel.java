9214dbddf9a4a7818735a91007454e6cb967cc26
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-27 12:37:20 -0300

    refactor: update  CollaboratorsModel class

--- src/main/java/com/lacertech/lacereats/database/model/CollaboratorsModel.java
+++ src/main/java/com/lacertech/lacereats/database/model/CollaboratorsModel.java
@@ -1,7 +1,5 @@
 package com.lacertech.lacereats.database.model;
 
-import java.util.Date;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,212 +8,138 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Date;
 
+/**
+ * This class represents a collaborator model entity.
+ * It is used to store information about collaborators in the application.
+ *
+ * @author Italo Lacerda
+ * @since 2.0 
+ */
 @Entity
 @Table(name = "collaborators")
 public class CollaboratorsModel {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_roles")
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_roles")
+  private Integer id;
 
-    @Column(length = 20,
-            name = "first_name", 
-            nullable = false)
-    private String firstName;
+  @Column(length = 20, name = "first_name", nullable = false)
+  private String firstName;
 
-    @Column(length = 60, 
-            name = "last_name", 
-            nullable = false)
-    private String lastName;
+  @Column(length = 60, name = "last_name", nullable = false)
+  private String lastName;
 
-    @Column(length = 45, 
-            name = "personal_email", 
-            nullable = false, 
-            unique = true)
-    private String personalEmail;
+  @Column(length = 45, name = "personal_email", nullable = false, unique = true)
+  private String personalEmail;
 
-    @Column(length = 45, 
-            name = "busiess_email", 
-            unique = true)
-    private String busiessEmail;
+  @Column(length = 45, name = "busiess_email", unique = true)
+  private String busiessEmail;
 
-    @Column(length = 11, 
-            name = "cell_phone", 
-            nullable = false)
-    private Long cellPhone;
+  @Column(length = 11, name = "cell_phone", nullable = false)
+  private Long cellPhone;
 
-    @Column(name = "dateOfBirth", 
-            nullable = false)
-    private Date dateOfBirth;
+  @Column(name = "dateOfBirth", nullable = false)
+  private Date dateOfBirth;
 
-    @Column(length = 11, 
-            name = "cpf", 
-            nullable = false, 
-            unique = true)
-    private Long cpf;
+  @Column(length = 11, name = "cpf", nullable = false, unique = true)
+  private Long cpf;
 
-    @Column(length = 11, 
-            name = "rg", 
-            unique = true)
-    private Integer rg;
+  @Column(length = 11, name = "rg", unique = true)
+  private Integer rg;
 
-    @Column(nullable = false)
-    private Boolean status;
+  @Column(nullable = false)
+  private Boolean status;
 
-    @ManyToOne
-    @JoinColumn(name = "id_role", 
-                nullable = false)
-    private RolesModel role;
-
-    @ManyToOne
-    @JoinColumn(name = "id_adresses")
-    private AdressesModel adresses;
-
-    public CollaboratorsModel() {}
-
-    public CollaboratorsModel(
-        String firstName,
-        String lastName,
-        String personalEmail,
-        String busiessEmail,
-        Long cellPhone,
-        Date dateOfBirth,
-        Long cpf,
-        Integer rg,
-        Boolean status,
-        RolesModel role) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.personalEmail = personalEmail;
-        this.busiessEmail = busiessEmail;
-        this.cellPhone = cellPhone;
-        this.dateOfBirth = dateOfBirth;
-        this.cpf = cpf;
-        this.rg = rg;
-        this.status = status;
-        this.role = role;
-    }
-
-    public CollaboratorsModel(
-        Integer id,
-        String firstName,
-        String lastName,
-        String personalEmail,
-        String busiessEmail,
-        Long cellPhone,
-        Date dateOfBirth,
-        Long cpf,
-        Integer rg,
-        Boolean status,
-        RolesModel role) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.personalEmail = personalEmail;
-        this.busiessEmail = busiessEmail;
-        this.cellPhone = cellPhone;
-        this.dateOfBirth = dateOfBirth;
-        this.cpf = cpf;
-        this.rg = rg;
-        this.status = status;
-        this.role = role;
-    }
-
-    public AdressesModel getAdresses() {
-        return adresses;
-    }
-
-    public void setAdresses(AdressesModel adresses) {
-        this.adresses = adresses;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPersonalEmail() {
-        return personalEmail;
-    }
-
-    public void setPersonalEmail(String personalEmail) {
-        this.personalEmail = personalEmail;
-    }
-
-    public String getBusiessEmail() {
-        return busiessEmail;
-    }
-
-    public void setBusiessEmail(String busiessEmail) {
-        this.busiessEmail = busiessEmail;
-    }
-
-    public Long getCellPhone() {
-        return cellPhone;
-    }
-
-    public void setCellPhone(Long cellPhone) {
-        this.cellPhone = cellPhone;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public Long getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(Long cpf) {
-        this.cpf = cpf;
-    }
-
-    public Integer getRg() {
-        return rg;
-    }
-
-    public void setRg(Integer rg) {
-        this.rg = rg;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public RolesModel getRole() {
-        return role;
-    }
-
-    public void setRole(RolesModel role) {
-        this.role = role;
-    }
+  @ManyToOne
+  @JoinColumn(name = "id_role", 
+              nullable = false)
+  private RolesModel role;  
+  
+  @ManyToOne
+  @JoinColumn(name = "id_adresses")
+  private AdressesModel adresses;  
+  
+  public CollaboratorsModel() {}  
+  
+  /**
+   * Constructor for creating a new collaborator.
+   *
+   * @param firstName     The first name of the collaborator.
+   * @param lastName      The last name of the collaborator.
+   * @param personalEmail The personal email of the collaborator.
+   * @param busiessEmail  The business email of the collaborator.
+   * @param cellPhone     The cell phone number of the collaborator.
+   * @param dateOfBirth   The date of birth of the collaborator.
+   * @param cpf           The CPF of the collaborator.
+   * @param rg            The RG of the collaborator.
+   * @param status        The status of the collaborator.
+   * @param role          The role of the collaborator.
+   */
+  public CollaboratorsModel(
+      String firstName,
+      String lastName,
+      String personalEmail,
+      String busiessEmail,
+      Long cellPhone,
+      Date dateOfBirth,
+      Long cpf,
+      Integer rg,
+      Boolean status,
+      RolesModel role
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.personalEmail = personalEmail;
+    this.busiessEmail = busiessEmail;
+    this.cellPhone = cellPhone;
+    this.dateOfBirth = dateOfBirth;
+    this.cpf = cpf;
+    this.rg = rg;
+    this.status = status;
+    this.role = role;
+  } 
+  
+  /**
+   * Constructor for creating a collaborator with an existing ID.
+   *
+   * @param id            The unique identifier for the collaborator.
+   * @param firstName     The first name of the collaborator.
+   * @param lastName      The last name of the collaborator.
+   * @param personalEmail The personal email of the collaborator.
+   * @param busiessEmail  The business email of the collaborator.
+   * @param cellPhone     The cell phone number of the collaborator.
+   * @param dateOfBirth   The date of birth of the collaborator.
+   * @param cpf           The CPF of the collaborator.
+   * @param rg            The RG of the collaborator.
+   * @param status        The status of the collaborator.
+   * @param role          The role of the collaborator.
+   */
+  public CollaboratorsModel(
+      Integer id,
+      String firstName,
+      String lastName,
+      String personalEmail,
+      String busiessEmail,
+      Long cellPhone,
+      Date dateOfBirth,
+      Long cpf,
+      Integer rg,
+      Boolean status,
+      RolesModel role
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.personalEmail = personalEmail;
+    this.busiessEmail = busiessEmail;
+    this.cellPhone = cellPhone;
+    this.dateOfBirth = dateOfBirth;
+    this.cpf = cpf;
+    this.rg = rg;
+    this.status = status;
+    this.role = role;
+  }
 }
