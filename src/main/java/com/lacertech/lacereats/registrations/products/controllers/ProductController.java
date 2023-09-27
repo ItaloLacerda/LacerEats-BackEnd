f29c351b96836e7337d19b1c7d5026e55ecc6a7d
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-27 01:28:30 -0300

    feat: update ProductController

--- src/main/java/com/lacertech/lacereats/registrations/products/controllers/ProductController.java
+++ src/main/java/com/lacertech/lacereats/registrations/products/controllers/ProductController.java
@@ -1,8 +1,14 @@
 package com.lacertech.lacereats.registrations.products.controllers;
 
+import com.lacertech.lacereats.controllers.interfaces.CommonController;
+import com.lacertech.lacereats.dto.productDTO.ProductDTO;
+import com.lacertech.lacereats.dto.productDTO.ProductResponseDTO;
+import com.lacertech.lacereats.registrations.products.services.ProductService;
+import jakarta.validation.Valid;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,41 +18,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lacertech.lacereats.dto.productDTO.ProductDTO;
-import com.lacertech.lacereats.registrations.products.services.ProductService;
-
+/**
+ * This class represents a controller for handling product-related HTTP requests and responses.
+ *
+ * @author Italo Lacerda
+ * @since 2.0 
+ */
 @RestController
 @RequestMapping(path = "/registrations/product")
-public class ProductController {
+public class ProductController implements CommonController<ProductResponseDTO, ProductDTO> {
 
-    @Autowired
-    private ProductService productService;
-    
-    @GetMapping
-    public List<ProductDTO> searchAll(){
-        return productService.searchAll();
-    }
+  @Autowired
+  private ProductService productService;
+  
+  @GetMapping
+  public ResponseEntity<List<ProductResponseDTO>> searchAll() {
+    return new ResponseEntity<>(productService.searchAll(), HttpStatus.OK);
+  }
 
-    @PostMapping
-    public void register(@RequestBody ProductDTO product) {
-        productService.create(product);
-        return;
-    }
+  @PostMapping
+  public ResponseEntity<ProductResponseDTO> register(@RequestBody @Valid ProductDTO product) {
+    ProductResponseDTO newProduct = productService.post(product);
+    return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+  }
 
-    @GetMapping("/{id}")
-    public ProductDTO searchById(@PathVariable Integer id) {
-        return productService.searchById(id);
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<ProductResponseDTO> searchById(@PathVariable Integer id) {
+    ProductResponseDTO product = productService.searchById(id); 
+    return new ResponseEntity<>(product, HttpStatus.OK); 
+  }
 
-    @PutMapping
-    public void update(@RequestBody ProductDTO product) {
-        productService.update(product);
-        return;
-    }
-    
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id) {
-        productService.delete(id);
-        return;
-    }
+  @PutMapping("/{id}")
+  public ResponseEntity<ProductResponseDTO> update(
+      @PathVariable Integer id,
+      @RequestBody ProductDTO product
+  ) {
+    ProductResponseDTO updatedProduct = productService.update(id, product);
+    return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+  }
+  
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Integer id) {
+    productService.delete(id);
+    return ResponseEntity.noContent().build();
+  }
 }
