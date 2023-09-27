8e0cffc76dc67ef38c35f28e09a4972c5b1ab29c
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-27 01:27:33 -0300

    style: fix code style of CommonController class

--- src/main/java/com/lacertech/lacereats/controllers/interfaces/CommonController.java
+++ src/main/java/com/lacertech/lacereats/controllers/interfaces/CommonController.java
@@ -1,14 +1,24 @@
 package com.lacertech.lacereats.controllers.interfaces;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 
+/**
+ * This interface defines common methods for controllers that handle HTTP 
+ * requests and responses for resources.
+ *
+ * @param <ResponseDTO> The response DTO type representing the resource.
+ * @param <DTO> The DTO type used for creating or updating the resource.
+ */
 public interface CommonController<ResponseDTO, DTO> {
 
-    ResponseEntity<List<ResponseDTO>> searchAll();
-    ResponseEntity<ResponseDTO> register(DTO dto);
-    ResponseEntity<ResponseDTO> searchById(Integer id);
-    ResponseEntity<ResponseDTO> update(Integer id, DTO dto);
-    ResponseEntity<Void> delete(Integer id);
+  ResponseEntity<List<ResponseDTO>> searchAll();
+
+  ResponseEntity<ResponseDTO> register(DTO dto);
+
+  ResponseEntity<ResponseDTO> searchById(Integer id);
+
+  ResponseEntity<ResponseDTO> update(Integer id, DTO dto);
+
+  ResponseEntity<Void> delete(Integer id);
 }
