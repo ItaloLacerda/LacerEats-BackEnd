ad82c46f8b1579aa6110394567d2733ca6f6e395
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-27 00:43:04 -0300

    feat: Update ProductRepository

--- src/main/java/com/lacertech/lacereats/database/repository/ProductRepository.java
+++ src/main/java/com/lacertech/lacereats/database/repository/ProductRepository.java
@@ -1,10 +1,17 @@
 package com.lacertech.lacereats.database.repository;
 
+import com.lacertech.lacereats.database.model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.lacertech.lacereats.database.model.ProductModel;
-
+/**
+ * This interface defines a repository for managing ProductModel entities.
+ * It extends the JpaRepository interface, which provides basic CRUD operations for ProductModel.
+ *
+ * @author Italo Lacerda
+ */
 public interface ProductRepository extends JpaRepository<ProductModel, Integer> {
-    
+  boolean existsByDescription(String description);
+  
+  boolean existsByBarCode(String barCode);
 }
 
