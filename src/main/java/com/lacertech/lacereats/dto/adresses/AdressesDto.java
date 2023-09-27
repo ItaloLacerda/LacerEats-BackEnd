bcfcd1e1c590b31fc9c1753c3ce728ae9e22dd34
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-09-27 14:59:02 -0300

    refactor: refactor AddressesDto

---
+++ src/main/java/com/lacertech/lacereats/dto/adresses/AdressesDto.java
@@ -0,0 +1,45 @@
+package com.lacertech.lacereats.dto.adresses;
+
+import com.lacertech.lacereats.database.model.AdressesModel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * This class represents a Data Transfer Object (DTO) for address information.
+ * It is used to encapsulate address data for input and output in the application.
+ *
+ * @author Italo Lacerda
+ * @since 2.0
+ */
+public record AdressesDto(
+    
+    @NotBlank(message = "{zipCode.not.blank}")
+    @Size(max = 8, min = 8, message = "{zipCode.size}")
+    String zipCode,
+
+    @NotBlank(message = "{city.not.blank}")
+    String city,
+
+    @NotBlank(message = "{publicPlace.not.blank}")
+    String publicPlace,
+
+    @NotBlank(message = "{neighborhood.not.blank}")
+    String neighborhood,
+
+    @NotBlank(message = "{state.not.blank}")
+    String state
+) {
+
+  /**
+   * Constructor to create an AdressesDTO from an AdressesModel.
+   *
+   * @param adresses The AdressesModel from which to create the DTO.
+   */
+  public AdressesDto(AdressesModel adresses) {
+    this(adresses.getZipCode(),
+        adresses.getCity(),
+        adresses.getPublicPlace(),
+        adresses.getNeighborhood(),
+        adresses.getState());
+  }
+}
