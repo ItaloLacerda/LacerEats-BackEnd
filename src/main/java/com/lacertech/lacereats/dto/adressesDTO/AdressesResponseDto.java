f7eeac92dd53c9d374552c90621670d2ddeafc84
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-12-12 15:57:41 -0300

    feat: create AddressResponseDto

---
+++ src/main/java/com/lacertech/lacereats/dto/adressesDTO/AdressesResponseDto.java
@@ -0,0 +1,50 @@
+package com.lacertech.lacereats.dto.adressesDTO;
+
+import com.lacertech.lacereats.database.model.AdressesModel;
+
+/**
+ * This class represents a Data Transfer Object (DTO) for responding with address information.
+ * It is used to encapsulate address data for sending as a response in the application.
+ *
+ * @author Italo Lacerda
+ * @since 2.0
+ */
+public record AdressesResponseDto(
+    Integer id,
+    String zipCode,
+    String city,
+    String publicPlace,
+    String neighborhood,
+    String state
+) {
+
+  /**
+   * Constructor to create an AdressesResponseDto from an AdressesModel.
+   *
+   * @param adresses The AdressesModel from which to create the DTO.
+   */
+  public AdressesResponseDto(AdressesModel adresses) {
+    this(adresses.getId(),
+        adresses.getZipCode(),
+        adresses.getCity(),
+        adresses.getPublicPlace(),
+        adresses.getNeighborhood(),
+        adresses.getState());
+  }
+
+  /**
+   * Static factory method to create an AdressesResponseDto from an AdressesModel.
+   *
+   * @param adresses The AdressesModel from which to create the DTO.
+   * @return An instance of AdressesResponseDto.
+   */
+  public static AdressesResponseDto turnsIntoDto(AdressesModel adresses) {
+    return new AdressesResponseDto(
+        adresses.getId(),
+        adresses.getZipCode(),
+        adresses.getCity(),
+        adresses.getPublicPlace(),
+        adresses.getNeighborhood(),
+        adresses.getState());
+  }
+}
