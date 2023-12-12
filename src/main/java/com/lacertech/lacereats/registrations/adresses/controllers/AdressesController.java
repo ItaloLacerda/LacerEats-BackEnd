79f1733302cb08fc6341847efd9a9ae409bebeec
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-12-12 16:03:56 -0300

    feat: Update AdressesController

--- src/main/java/com/lacertech/lacereats/registrations/adresses/controllers/AdressesController.java
+++ src/main/java/com/lacertech/lacereats/registrations/adresses/controllers/AdressesController.java
@@ -3,6 +3,8 @@ package com.lacertech.lacereats.registrations.adresses.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,41 +14,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lacertech.lacereats.controllers.interfaces.CommonController;
 import com.lacertech.lacereats.dto.adressesDTO.AdressesDTO;
+import com.lacertech.lacereats.dto.adressesDTO.AdressesResponseDto;
 import com.lacertech.lacereats.registrations.adresses.services.AdressesService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/registrations/adresses")
-public class AdressesController {
+public class AdressesController implements CommonController<AdressesResponseDto, AdressesDTO> {
 
     @Autowired
     private AdressesService adressesService;
     
     @GetMapping
-    public List<AdressesDTO> searchAll(){
-        return adressesService.searchAll();
+    public ResponseEntity<List<AdressesResponseDto>> searchAll(){
+        return new ResponseEntity<>(adressesService.searchAll(), HttpStatus.OK);
     }
 
     @PostMapping
-    public void register(@RequestBody AdressesDTO adresses) {
-        adressesService.create(adresses);
-        return;
+    public ResponseEntity<AdressesResponseDto> register(@RequestBody AdressesDTO adresses) {
+        AdressesResponseDto creatingNewAdresses = adressesService.create(adresses); 
+        return new ResponseEntity<>(creatingNewAdresses , HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public AdressesDTO searchById(@PathVariable Integer id) {
-        return adressesService.searchById(id);
+    public ResponseEntity<AdressesResponseDto> searchById(@PathVariable Integer id) {
+        AdressesResponseDto adresses = adressesService.searchById(id); 
+        return new ResponseEntity<>(adresses, HttpStatus.OK);
     }
 
-    @PutMapping
-    public void update(@RequestBody AdressesDTO adresses) {
-        adressesService.update(adresses);
-        return;
+    @PutMapping("/{id}")
+    public ResponseEntity<AdressesResponseDto> update(
+        @PathVariable 
+        Integer id,
+        @RequestBody
+        @Valid  
+        AdressesDTO adresses) {
+        
+        AdressesResponseDto adressesUpdate = adressesService.update(id, adresses);
+        return new ResponseEntity<>(adressesUpdate, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id) {
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         adressesService.delete(id);
-        return;
+        return ResponseEntity.noContent().build();
     }
 }
